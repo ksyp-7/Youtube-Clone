f477c0dd4d300d5e1ce05dae49bd0d736e790f74
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-10 22:09:46 +0530

    Search page
    created Search page componet

---
+++ youtube/src/SearchPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TuneIcon from '@material-ui/icons/Tune';
+import './Searchpage.css';
+
+export default function SearchPage() {
+    return (
+        <div className="SearchPage">
+            <div className="Serch_filter">
+                <TuneIcon />
+                <h2>Filter</h2>
+            </div>
+        </div>
+    )
+}
