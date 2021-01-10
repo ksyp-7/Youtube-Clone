0f181c84b4a88c16f1eddbb40b8e2d11e91bb5b9
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-10 10:47:34 +0530

    Recomanded
    One more component created!

---
+++ youtube/src/Recomand.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {  } from "./recomand.css";
+
+function Recomand() {
+    return (
+        <div className="recomandedvideos">
+            <h2>
+                this is where videos go
+            </h2>
+        </div>
+    )
+}
+
+export default Recomand
