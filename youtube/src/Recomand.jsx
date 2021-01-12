ced889b615ccf24ec35dfdc23dbd007c64ec176e
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-12 17:09:27 +0530

    Dummy data added to componet

--- youtube/src/Recomand.jsx
+++ youtube/src/Recomand.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import VideoCard from "./VideoCard";
-import {  } from "./recomand.css";
+import { } from "./recomand.css";
 
 function Recomand() {
     return (
@@ -9,13 +9,104 @@ function Recomand() {
                 Recomanded
             </h2>
             <div className="recomand_video">
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
+                <VideoCard
+                image="https://picsum.photos/200/300"
+                channel="Abc"
+                channelImage="https://picsum.photos/300/300 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/230/320"
+                channel="Abc"
+                channelImage="https://picsum.photos/301/100 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/211/350"
+                channel="Abc"
+                channelImage="https://picsum.photos/322/220 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/100/350"
+                channel="Abc"
+                channelImage="https://picsum.photos/345/852 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/242/450"
+                channel="Abc"
+                channelImage="https://picsum.photos/303/180 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/852/950"
+                channel="Abc"
+                channelImage="https://picsum.photos/321/525 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/521/125"
+                channel="Abc"
+                channelImage="https://picsum.photos/660/324 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard
+                image="https://picsum.photos/200/300"
+                channel="Abc"
+                channelImage="https://picsum.photos/300/300 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/230/320"
+                channel="Abc"
+                channelImage="https://picsum.photos/301/100 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/211/350"
+                channel="Abc"
+                channelImage="https://picsum.photos/322/220 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/100/350"
+                channel="Abc"
+                channelImage="https://picsum.photos/345/852 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/242/450"
+                channel="Abc"
+                channelImage="https://picsum.photos/303/180 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/852/950"
+                channel="Abc"
+                channelImage="https://picsum.photos/321/525 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
+                <VideoCard 
+                image="https://picsum.photos/521/125"
+                channel="Abc"
+                channelImage="https://picsum.photos/660/324 "
+                title="Become a Web Devloper in 10! minutes | 2019/2020"
+                views="15.08M Views"
+                timestamp="5 daays ago"/>
 
             </div>
         </div>
