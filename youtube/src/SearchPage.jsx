4dd9f922be31a4f7c6532cde70551d43f676409e
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-12 17:27:03 +0530

    Dummy data added to SearchPage

--- youtube/src/SearchPage.jsx
+++ youtube/src/SearchPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TuneIcon from '@material-ui/icons/Tune';
 import ChnnelRow from './ChnnelRow';
+import VideoRow from './VideoRow';
 import './Searchpage.css';
 
 export default function SearchPage() {
@@ -11,8 +12,18 @@ export default function SearchPage() {
                 <h2>Filter</h2>
             </div>
             <hr />
-            <ChnnelRow />
+            <ChnnelRow
+                image="https://picsum.photos/242/450"
+                chnnel="Shukl Kashyap"
+                verifed
+                subs="215K"
+                numofvideos={651}
+                description="This is description!.This is description!.This is description!.
+                This is description!.This is description!.This is description!."
+            
+            />
             <hr />
+            <VideoRow />
         </div>
     )
 }
